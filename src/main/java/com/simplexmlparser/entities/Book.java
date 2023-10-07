0dcdc98572b221a83399bd28e6a2017a6e929d5d
DenisKP <denis.buserski@gmail.com>
2023-10-07 22:47:33 +0300

    Create Book constructor + Getters and Setters

--- src/main/java/com/simplexmlparser/entities/Book.java
+++ src/main/java/com/simplexmlparser/entities/Book.java
@@ -18,4 +18,62 @@ public class Book {
     @Column(name = "publish_date")
     private LocalDate publishDate;
     private String description;
+
+    public Book() {}
+    public Book(String id, String title, Genre genre, BigDecimal price, LocalDate publishDate, String description) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.price = price;
+        this.publishDate = publishDate;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDate getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(LocalDate publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
