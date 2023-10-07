f59b2970d46135e66ebd85cb40532029da4fb68e
DenisKP <denis.buserski@gmail.com>
2023-10-07 22:44:16 +0300

    Create BookRepository

---
+++ src/main/java/com/simplexmlparser/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package com.simplexmlparser.repositories;
+
+import com.simplexmlparser.entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Integer, Book> {
+}
