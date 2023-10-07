f46cbd91f4bdb67fa0607ac06c2d609385bc418d
DenisKP <denis.buserski@gmail.com>
2023-10-07 22:10:13 +0300

    Update XmlReader class + Add Id annotation to Book class

--- src/main/java/com/simplexmlparser/XmlReader.java
+++ src/main/java/com/simplexmlparser/XmlReader.java
@@ -1,4 +1,58 @@
 package com.simplexmlparser;
 
-public class XmlReader {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
+
+@Component
+public class XmlReader implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+        try {
+
+            File file = new File("src/main/resources/books.xml"); // Creating a constructor of file class and parsing an XML file
+
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance(); // An instance of factory that gives a document builder
+
+            DocumentBuilder documentBuilder = dbf.newDocumentBuilder(); // An instance of builder to parse the specified xml file
+            Document document = documentBuilder.parse(file);
+            document.getDocumentElement().normalize();
+
+            System.out.println("Root element: " + document.getDocumentElement().getNodeName() + System.lineSeparator());
+            NodeList nodeList = document.getElementsByTagName("book");
+
+            for (int i = 0; i < nodeList.getLength(); i++) { // NodeList is not iterable, so we are using for loop
+                Node node = nodeList.item(i);
+
+                if (i == 0) {
+                    System.out.println("Node Name :" + node.getNodeName() + System.lineSeparator());
+                }
+
+
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) node;
+                    System.out.println("Author name: " + eElement.getElementsByTagName("author").item(0).getTextContent());
+                    System.out.println("Title: " + eElement.getElementsByTagName("title").item(0).getTextContent());
+                    System.out.println("Genre: " + eElement.getElementsByTagName("genre").item(0).getTextContent());
+                    System.out.println("Price: " + eElement.getElementsByTagName("price").item(0).getTextContent());
+                    System.out.println("Publish date: " + eElement.getElementsByTagName("publish_date").item(0).getTextContent());
+                    System.out.println("Description: " + eElement.getElementsByTagName("description").item(0).getTextContent());
+                    System.out.println();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
--- src/main/java/com/simplexmlparser/entities/Book.java
+++ src/main/java/com/simplexmlparser/entities/Book.java
@@ -3,12 +3,14 @@ package com.simplexmlparser.entities;
 import com.simplexmlparser.enums.Genre;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity(name = "books")
 public class Book {
+    @Id
     private String id;
     private String title;
     private Genre genre;
