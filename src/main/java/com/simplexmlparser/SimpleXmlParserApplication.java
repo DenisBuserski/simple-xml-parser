1db533bfd9c8d0ebc8974ce1734c631c7709021d
DenisKP <denis.buserski@gmail.com>
2023-10-07 18:18:07 +0300

    Start project

---
+++ src/main/java/com/simplexmlparser/SimpleXmlParserApplication.java
@@ -0,0 +1,13 @@
+package com.simplexmlparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleXmlParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleXmlParserApplication.class, args);
+	}
+
+}
